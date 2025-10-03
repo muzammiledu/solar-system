a2059bf5ea7a7cc635b7c6b85c8fc9b2e1cee0a0
muzammiledu <87443008+muzammiledu@users.noreply.github.com>
2025-10-03 11:14:41 +0530

    update app

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const path = require('path');
 const express = require('express');
 const OS = require('os');
@@ -11,18 +12,19 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '/')));
 app.use(cors());
 
-mongoose.connect(process.env.MONGO_URI, {
-    user: process.env.MONGO_USERNAME,
-    pass: process.env.MONGO_PASSWORD,
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}, function(err) {
-    if (err) {
+(async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI, {
+            user: process.env.MONGO_USERNAME,
+            pass: process.env.MONGO_PASSWORD,
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        // console.log("MongoDB Connection Successful");
+    } catch (err) {
         console.log("error!! " + err);
-    } else {
-      //  console.log("MongoDB Connection Successful");
     }
-});
+})();
 
 var Schema = mongoose.Schema;
 
