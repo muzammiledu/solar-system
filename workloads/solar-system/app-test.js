9bc61a680c83d14813426275114856998a5c346e
muzammiledu <87443008+muzammiledu@users.noreply.github.com>
2025-10-05 14:34:10 +0530

    pushing worklaod folder

---
+++ workloads/solar-system/app-test.js
@@ -0,0 +1,98 @@
+let mongoose = require("mongoose");
+let server = require("./app");
+let chai = require("chai");
+let chaiHttp = require("chai-http");
+
+// Assertion 
+chai.should();
+chai.use(chaiHttp); 
+
+describe('Planets API Suite', () => {
+
+    describe('Fetching Planet Details', () => {
+        it('it should fetch a planet named Mercury', (done) => {
+            let payload = {
+                id: 1
+            }
+          chai.request(server)
+              .post('/planet')
+              .send(payload)
+              .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.have.property('id').eql(1);
+                    res.body.should.have.property('name').eql('Mercury');
+                done();
+              });
+        });
+
+        // Other tests for planets...
+
+    });
+
+    describe('Fetching Invalid Planet', () => {
+        it('it should return error for invalid planet ID', (done) => {
+            let payload = {
+                id: 10 // Invalid ID
+            }
+            chai.request(server)
+                .post('/planet')
+                .send(payload)
+                .end((err, res) => {
+                    res.should.have.status(400);
+                    res.body.errors[0].msg.should.equal('ID must be an integer between 0 and 9');
+                    done();
+                });
+        });
+    });
+});
+
+describe('Testing Other Endpoints', () => {
+
+    describe('it should fetch OS Details', () => {
+        it('it should fetch OS details', (done) => {
+          chai.request(server)
+              .get('/os')
+              .end((err, res) => {
+                    res.should.have.status(200);
+                done();
+              });
+        });
+    });
+
+    describe('Root Endpoint', () => {
+        it('should serve index.html', (done) => {
+            chai.request(server)
+                .get('/')
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.type.should.equal('text/html');
+                    done();
+                });
+        });
+    });
+
+    describe('it should fetch Live Status', () => {
+        it('it checks Liveness endpoint', (done) => {
+          chai.request(server)
+              .get('/live')
+              .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.have.property('status').eql('live');
+                done();
+              });
+        });
+    });
+
+    describe('it should fetch Ready Status', () => {
+        it('it checks Readiness endpoint', (done) => {
+          chai.request(server)
+              .get('/ready')
+              .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.have.property('status').eql('ready');
+                done();
+              });
+        });
+    });
+
+});
---
+++ workloads/solar-system/app.js
@@ -0,0 +1,87 @@
+require('dotenv').config();
+const path = require('path');
+const express = require('express');
+const OS = require('os');
+const bodyParser = require('body-parser');
+const mongoose = require("mongoose");
+const app = express();
+const cors = require('cors');
+const { body, validationResult } = require('express-validator');
+
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, '/')));
+app.use(cors());
+
+(async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI, {
+            user: process.env.MONGO_USERNAME,
+            pass: process.env.MONGO_PASSWORD,
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        // console.log("MongoDB Connection Successful");
+    } catch (err) {
+        console.log("error!! " + err);
+    }
+})();
+
+var Schema = mongoose.Schema;
+
+var dataSchema = new Schema({
+    name: String,
+    id: Number,
+    description: String,
+    image: String,
+    velocity: String,
+    distance: String
+});
+var planetModel = mongoose.model('planets', dataSchema);
+
+app.post('/planet', 
+    body('id').isInt({ min: 0, max: 9 }).withMessage('ID must be an integer between 0 and 9'),
+    async function(req, res) {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        try {
+            const planetData = await planetModel.findOne({ id: req.body.id });
+            res.send(planetData);
+        } catch (err) {
+            res.status(500).send("Error in Planet Data");
+        }
+    }
+);
+
+app.get('/', async (req, res) => {
+    res.sendFile(path.join(__dirname, '/', 'index.html'));
+});
+
+app.get('/os', function(req, res) {
+    res.setHeader('Content-Type', 'application/json');
+    res.send({
+        "os": OS.hostname(),
+        "env": process.env.NODE_ENV
+    });
+});
+
+app.get('/live', function(req, res) {
+    res.setHeader('Content-Type', 'application/json');
+    res.send({
+        "status": "live"
+    });
+});
+
+app.get('/ready', function(req, res) {
+    res.setHeader('Content-Type', 'application/json');
+    res.send({
+        "status": "ready"
+    });
+});
+
+app.listen(3000, () => {
+    console.log("Server successfully running on port - " + 3000);
+});
+
+module.exports = app;
